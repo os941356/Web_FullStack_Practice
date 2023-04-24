71fbd2755dc8bf9e483afc337d844f1c6eb9687d
hongyaung <hongyaung@gmail.com>
2023-04-24 17:02:44 +0800

    0424_2

--- 20230407VUEE/vite.config.ts
+++ 20230407VUEE/vite.config.ts
@@ -6,6 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:"/Web_FullStack_Practice/20230407VUEE/dist/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
